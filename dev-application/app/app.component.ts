509c731d36b557f52e2ef84f021768ffcc8b7f7c
vitalii <vm.mishchenko@gmail.com>
2017-09-30 09:57:01 +0300

    update roadmap

--- dev-application/app/app.component.ts
+++ dev-application/app/app.component.ts
@@ -17,167 +17,189 @@ export class AppComponent {
     };
 
     wallPlan: IWallDefinition = {
-        'bricks': [
+        "bricks": [
             {
-                'id': 'b631913d-84c5-5c7b-ecf0-714453a883f2',
-                'tag': 'header',
-                'data': {
-                    'text': 'WALL'
+                "id": "b631913d-84c5-5c7b-ecf0-714453a883f2",
+                "tag": "header",
+                "data": {
+                    "text": "WALL"
                 },
-                'meta': {}
+                "meta": {}
             },
             {
-                'id': '13e25e42-ae37-0ee6-8bf2-fd7f78bf8762',
-                'tag': 'text',
-                'data': {
-                    'text': 'The goal of the project is to create extensible web editor which provides clear and simple API for adding new type of editors (bricks) based on Angular components.'
+                "id": "13e25e42-ae37-0ee6-8bf2-fd7f78bf8762",
+                "tag": "text",
+                "data": {
+                    "text": "The goal of the project is to create extensible web editor which provides clear and simple API for adding new type of editors (bricks) based on Angular components."
                 },
-                'meta': {}
+                "meta": {}
             },
             {
-                'id': '89399ff0-2d1a-bbd3-68ae-a55045571c4b',
-                'tag': 'text',
-                'data': {
-                    'text': ''
+                "id": "89399ff0-2d1a-bbd3-68ae-a55045571c4b",
+                "tag": "text",
+                "data": {
+                    "text": ""
                 },
-                'meta': {}
+                "meta": {}
             },
             {
-                'id': 'cf7a2e00-5016-8b6d-b238-78c4b83b4468',
-                'tag': 'img',
-                'data': {
-                    'src': 'https://cdn3.volusion.com/mache.udhvk/v/vspfiles/photos/M8994-PARENT-2.jpg'
+                "id": "cf7a2e00-5016-8b6d-b238-78c4b83b4468",
+                "tag": "img",
+                "data": {
+                    "src": "https://cdn3.volusion.com/mache.udhvk/v/vspfiles/photos/M8994-PARENT-2.jpg"
                 },
-                'meta': {}
+                "meta": {}
             },
             {
-                'id': '4f3ce9ed-bebc-17e7-c23a-2cc64027ed09',
-                'tag': 'header',
-                'data': {
-                    'text': 'Roadmap'
+                "id": "4f3ce9ed-bebc-17e7-c23a-2cc64027ed09",
+                "tag": "header",
+                "data": {
+                    "text": "Roadmap"
                 },
-                'meta': {}
+                "meta": {}
             },
             {
-                'id': '4b089695-bad3-06eb-f782-239c340864f3',
-                'tag': 'text',
-                'data': {
-                    'text': '- drag\'n\'drop support'
+                "id": "878eca6b-5c7b-0cfa-c9ad-71481879d3e3",
+                "tag": "text",
+                "data": {
+                    "text": "- support few editors on the page"
                 },
-                'meta': {}
+                "meta": {}
             },
             {
-                'id': '878eca6b-5c7b-0cfa-c9ad-71481879d3e3',
-                'tag': 'text',
-                'data': {
-                    'text': '- support few editors on the page'
+                "id": "30ee9fac-991f-ec6f-5b08-801d00d760a4",
+                "tag": "header",
+                "data": {
+                    "text": "Supported tags"
                 },
-                'meta': {}
+                "meta": {}
             },
             {
-                'id': '30ee9fac-991f-ec6f-5b08-801d00d760a4',
-                'tag': 'header',
-                'data': {
-                    'text': 'Supported tags'
+                "id": "4cf44a15-d08e-399d-38ba-c051a36f6574",
+                "tag": "text",
+                "data": {
+                    "text": "/h header brick"
                 },
-                'meta': {}
+                "meta": {}
             },
             {
-                'id': '4cf44a15-d08e-399d-38ba-c051a36f6574',
-                'tag': 'text',
-                'data': {
-                    'text': '/h header brick'
+                "id": "56be0630-1cd8-4997-b69c-e40421cf79da",
+                "tag": "text",
+                "data": {
+                    "text": "/img image brick"
                 },
-                'meta': {}
+                "meta": {}
             },
             {
-                'id': '56be0630-1cd8-4997-b69c-e40421cf79da',
-                'tag': 'text',
-                'data': {
-                    'text': '/img image brick'
+                "id": "f936a755-108a-a259-1f00-3c7bdd5d5ced",
+                "tag": "header",
+                "data": {
+                    "text": "Navigation"
                 },
-                'meta': {}
+                "meta": {}
             },
             {
-                'id': 'f936a755-108a-a259-1f00-3c7bdd5d5ced',
-                'tag': 'header',
-                'data': {
-                    'text': 'Navigation'
+                "id": "f796f546-57ce-49cb-9c1f-0479df9212b1",
+                "tag": "text",
+                "data": {
+                    "text": "Press `Escape` to enter to Selection mode"
                 },
-                'meta': {}
+                "meta": {}
             },
             {
-                'id': 'f796f546-57ce-49cb-9c1f-0479df9212b1',
-                'tag': 'text',
-                'data': {
-                    'text': 'Press `Escape` to enter to Selection mode'
+                "id": "f6333894-7e4d-2759-c425-9b7feda57801",
+                "tag": "text",
+                "data": {
+                    "text": "Press Shift to select few bricks"
                 },
-                'meta': {}
+                "meta": {}
             },
             {
-                'id': 'f6333894-7e4d-2759-c425-9b7feda57801',
-                'tag': 'text',
-                'data': {
-                    'text': 'Press Shift to select few bricks'
+                "id": "94339d03-f539-cded-bb5a-4273d05898d5",
+                "tag": "text",
+                "data": {
+                    "text": "- support readonly mode"
                 },
-                'meta': {}
+                "meta": {}
             },
             {
-                'id': '94339d03-f539-cded-bb5a-4273d05898d5',
-                'tag': 'text',
-                'data': {
-                    'text': '- support readonly mode'
+                "id": "7e69cf25-1860-995f-82f6-15850936977c",
+                "tag": "text",
+                "data": {
+                    "text": "- during text selection disable global selection"
                 },
-                'meta': {}
+                "meta": {}
+            },
+            {
+                "id": "d3700c3e-4bfc-cfe3-7838-dea3a49585c9",
+                "tag": "text",
+                "data": {
+                    "text": "- improve drag and drop handler"
+                },
+                "meta": {}
+            },
+            {
+                "id": "d4d99ab3-6b32-ce9e-397a-e5bdff8b0333",
+                "tag": "text",
+                "data": {
+                    "text": "- drag and drop few bricks"
+                },
+                "meta": {}
             }
         ],
-        'layout': {
-            'bricks': [
+        "layout": {
+            "bricks": [
                 {
-                    'columns': [
+                    "columns": [
                         {
-                            'bricks': [
+                            "bricks": [
                                 {
-                                    'id': 'b631913d-84c5-5c7b-ecf0-714453a883f2'
+                                    "id": "b631913d-84c5-5c7b-ecf0-714453a883f2"
                                 },
                                 {
-                                    'id': '13e25e42-ae37-0ee6-8bf2-fd7f78bf8762'
+                                    "id": "13e25e42-ae37-0ee6-8bf2-fd7f78bf8762"
                                 },
                                 {
-                                    'id': 'f936a755-108a-a259-1f00-3c7bdd5d5ced'
+                                    "id": "f936a755-108a-a259-1f00-3c7bdd5d5ced"
                                 },
                                 {
-                                    'id': 'f796f546-57ce-49cb-9c1f-0479df9212b1'
+                                    "id": "f796f546-57ce-49cb-9c1f-0479df9212b1"
                                 },
                                 {
-                                    'id': 'f6333894-7e4d-2759-c425-9b7feda57801'
+                                    "id": "f6333894-7e4d-2759-c425-9b7feda57801"
                                 },
                                 {
-                                    'id': '4f3ce9ed-bebc-17e7-c23a-2cc64027ed09'
+                                    "id": "4f3ce9ed-bebc-17e7-c23a-2cc64027ed09"
                                 },
                                 {
-                                    'id': '4b089695-bad3-06eb-f782-239c340864f3'
+                                    "id": "d3700c3e-4bfc-cfe3-7838-dea3a49585c9"
                                 },
                                 {
-                                    'id': '878eca6b-5c7b-0cfa-c9ad-71481879d3e3'
+                                    "id": "d4d99ab3-6b32-ce9e-397a-e5bdff8b0333"
                                 },
                                 {
-                                    'id': '94339d03-f539-cded-bb5a-4273d05898d5'
+                                    "id": "7e69cf25-1860-995f-82f6-15850936977c"
                                 },
                                 {
-                                    'id': '89399ff0-2d1a-bbd3-68ae-a55045571c4b'
+                                    "id": "878eca6b-5c7b-0cfa-c9ad-71481879d3e3"
                                 },
                                 {
-                                    'id': 'cf7a2e00-5016-8b6d-b238-78c4b83b4468'
+                                    "id": "94339d03-f539-cded-bb5a-4273d05898d5"
                                 },
                                 {
-                                    'id': '30ee9fac-991f-ec6f-5b08-801d00d760a4'
+                                    "id": "89399ff0-2d1a-bbd3-68ae-a55045571c4b"
                                 },
                                 {
-                                    'id': '4cf44a15-d08e-399d-38ba-c051a36f6574'
+                                    "id": "cf7a2e00-5016-8b6d-b238-78c4b83b4468"
                                 },
                                 {
-                                    'id': '56be0630-1cd8-4997-b69c-e40421cf79da'
+                                    "id": "30ee9fac-991f-ec6f-5b08-801d00d760a4"
+                                },
+                                {
+                                    "id": "4cf44a15-d08e-399d-38ba-c051a36f6574"
+                                },
+                                {
+                                    "id": "56be0630-1cd8-4997-b69c-e40421cf79da"
                                 }
                             ]
                         }
