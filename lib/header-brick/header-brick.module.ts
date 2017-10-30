60216a79bcdc5b7bf69ee3855858c42df59d609f
vm-mishchenko <vm.mishchenko@gmail.com>
2017-10-31 00:50:27 +0200

    Remove unnecessary lib dependency

--- lib/header-brick/header-brick.module.ts
+++ lib/header-brick/header-brick.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrickRegistry } from '../wall';
 import { HeaderBrickComponent } from './component/header-brick.component';
-import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
-    imports: [
-        BrowserModule,
-    ],
     exports: [HeaderBrickComponent],
     declarations: [HeaderBrickComponent],
     entryComponents: [HeaderBrickComponent]
