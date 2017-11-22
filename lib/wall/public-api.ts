7a28c1f8a077f519ace7eea26a7307d387d63b05
Vitalii Mishchenko <vm.mishchneko@gmail.com>
2017-11-22 13:33:57 +0200

    export BrickModel

--- lib/wall/public-api.ts
+++ lib/wall/public-api.ts
@@ -1,4 +1,5 @@
 export * from './wall.module';
+export * from './model/wall-brick.model';
 export * from './model/wall-model.factory';
 export * from './wall.interfaces';
 export * from './wall.tokens';
