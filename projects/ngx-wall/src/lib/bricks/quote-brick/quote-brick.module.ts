bdbfeb358b6b9f9c838d0571eabd862ddb4e5789
vm-mishchenko <vm.mishchenko@gmail.com>
2019-07-15 17:27:57 -0700

    Text, Header, Quote supports basic readonly mode.

--- projects/ngx-wall/src/lib/bricks/quote-brick/quote-brick.module.ts
+++ projects/ngx-wall/src/lib/bricks/quote-brick/quote-brick.module.ts
@@ -1,12 +1,14 @@
+import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {ContenteditableModule} from '../../modules/contenteditable/contenteditable.module';
 import {BrickRegistry} from '../../wall/wall';
 import {TextRepresentation} from '../base-text-brick/base-text-representation.class';
 import {QuoteBrickComponent} from './component/quote-brick.component';
-import {ContenteditableModule} from '../../modules/contenteditable/contenteditable.module';
 
 @NgModule({
     imports: [
+        CommonModule,
         FormsModule,
         ContenteditableModule
     ],
