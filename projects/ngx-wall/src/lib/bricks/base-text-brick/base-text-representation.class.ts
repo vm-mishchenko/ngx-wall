d8f6a9aa6109f5bdcfbc461c6732ddc7bf93b16a
Vitalii Mishchenko <vm.mishchenko@gmail.com>
2019-09-13 22:32:39 -0400

    Remove &nbsp; from text representation

--- projects/ngx-wall/src/lib/bricks/base-text-brick/base-text-representation.class.ts
+++ projects/ngx-wall/src/lib/bricks/base-text-brick/base-text-representation.class.ts
@@ -1,11 +1,13 @@
 import {IBrickSnapshot} from '../../wall/wall';
 import {IBaseTextState} from './base-text-state.interface';
 
+const REG_EXP_SPACE = new RegExp(/&nbsp;/, 'g');
+
 export class TextRepresentation {
     constructor(private brickSnapshot: IBrickSnapshot) {
     }
 
     getText() {
-        return (this.brickSnapshot.state as IBaseTextState).text;
+        return (this.brickSnapshot.state as IBaseTextState).text.replace(REG_EXP_SPACE, '');
     }
 }
