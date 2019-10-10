b558be6a2fb0985d5b2732a6948035fc6058da0e
Vitalii Mishchenko <vm.mishchenko@gmail.com>
2019-10-09 20:32:08 -0400

    Add highlight capabilities

--- projects/ngx-rich-input/src/lib/rich-input.component.ts
+++ projects/ngx-rich-input/src/lib/rich-input.component.ts
@@ -1,18 +1,158 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+import {baseKeymap} from 'prosemirror-commands';
+import {keymap} from 'prosemirror-keymap';
+import {DOMParser, DOMSerializer, Schema} from 'prosemirror-model';
+
+import {marks, nodes} from 'prosemirror-schema-basic';
+import {EditorState} from 'prosemirror-state';
+import {ReplaceStep} from 'prosemirror-transform';
+import {EditorView} from 'prosemirror-view';
 
 @Component({
   selector: 'rich-input',
   template: `
-      <p>
-          ngx-rich-input works!
-      </p>
-  `,
+      <div #container></div>`,
   styles: []
 })
 export class RichInputComponent implements OnInit {
+  @ViewChild('container') container: ElementRef;
+
+  @Input() config;
+
+  private view: EditorView;
+
   constructor() {
   }
 
   ngOnInit() {
+    const customSchema = new Schema({
+      nodes,
+      marks: {
+        ...marks,
+        ...this.config.marks.reduce((result, markConfig) => {
+          result[markConfig.name] = {
+            inclusive: false,
+            parseDOM: [{tag: markConfig.tag}],
+            toDOM: function toDOM() {
+              return [markConfig.tag, 0];
+            }
+          };
+
+          return result;
+        }, {})
+      },
+    });
+
+    const serializer = DOMSerializer.fromSchema(customSchema);
+
+    const domNode = document.createElement('div');
+    domNode.innerHTML = '';
+
+    const state = EditorState.create({
+      doc: DOMParser.fromSchema(customSchema).parse(domNode),
+      schema: customSchema,
+      plugins: [
+        keymap(baseKeymap)
+      ]
+    });
+
+    this.view = new EditorView(this.container.nativeElement, {
+      state,
+      dispatchTransaction: (transaction) => {
+        if (this.convertToMark(transaction)) {
+          return;
+        }
+
+        const newState = this.view.state.apply(transaction);
+
+        this.view.updateState(newState);
+      }
+    });
+  }
+
+  convertToMark(transaction) {
+    const convertToMark = this.config.marks.filter((markConfig) => {
+      return this.canConvertToTheMark(transaction, markConfig.wrapSymbol);
+    })[0];
+
+    if (!convertToMark) {
+      return false;
+    }
+
+    const leftNode = this.view.state.selection.$from.nodeBefore;
+    const cursorPosition = this.view.state.selection.$from.pos;
+    const leftOpenCharacterIndex = leftNode.text.indexOf(convertToMark.wrapSymbol);
+    const startPos = cursorPosition - leftNode.nodeSize + leftOpenCharacterIndex;
+
+    const tr = this.view.state.tr;
+    tr.delete(startPos, startPos + 1);
+
+    const mark = this.view.state.doc.type.schema.marks[convertToMark.name].create();
+    tr.addMark(startPos, tr.selection.$cursor.pos, mark);
+
+    this.view.dispatch(tr);
+
+    return true;
+  }
+
+  private canConvertToTheMark(transaction, symbol: string) {
+    return this.doesReplaceStepModifyOneTextNode(transaction) &&
+      this.isAddSymbol(transaction, symbol) &&
+      !this.doesBeforeNodeHaveMarks() &&
+      this.doesBeforeNodeHaveSymbol(symbol) &&
+      this.distanceToSymbolForBeforeNode(symbol);
+  }
+
+  private distanceToSymbolForBeforeNode(symbol: string): number {
+    const leftNode = this.view.state.selection.$from.nodeBefore;
+    const cursorPosition = this.view.state.selection.$from.pos;
+    const leftOpenCharacterIndex = leftNode.text.indexOf(symbol);
+    const startPos = cursorPosition - leftNode.nodeSize + leftOpenCharacterIndex;
+
+    return cursorPosition - startPos + 1;
+  }
+
+  private doesBeforeNodeHaveSymbol(symbol: string) {
+    const leftNode = this.view.state.selection.$from.nodeBefore;
+
+    if (!leftNode) {
+      return false;
+    }
+
+    const leftOpenCharacterIndex = leftNode.text.indexOf(symbol);
+
+    return leftNode.nodeSize && leftOpenCharacterIndex !== -1;
+  }
+
+  private doesBeforeNodeHaveMarks() {
+    const leftNode = this.view.state.selection.$from.nodeBefore;
+
+    return leftNode && leftNode.marks.length;
+  }
+
+  /**
+   * Check whether transaction has only ReplaceStep which modifies one text node.
+   */
+  private doesReplaceStepModifyOneTextNode(transaction) {
+    if (transaction.steps.length !== 1) {
+      return false;
+    }
+
+    const step = transaction.steps[0];
+
+    if (!(step instanceof ReplaceStep)) {
+      return false;
+    }
+
+    //
+    return step.slice.content.childCount === 1;
+  }
+
+  private isAddSymbol(transaction, symbol): boolean {
+    const step = transaction.steps[0];
+
+    const {text} = step.slice.content.firstChild;
+
+    return text && text[text.length - 1] === symbol;
   }
 }
--- src/app/rich-input/rich-input.component.ts
+++ src/app/rich-input/rich-input.component.ts
@@ -2,14 +2,30 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({
   template: `
-      <rich-input></rich-input>
+      <rich-input [config]="config"></rich-input>
   `,
-  styles: []
+  styleUrls: [`./rich-input.component.scss`]
 })
 export class RichInputComponent implements OnInit {
+  config = {
+    marks: [
+      {
+        name: 'snippet',
+        wrapSymbol: '`',
+        tag: 'snippet',
+      },
+      {
+        name: 'highlight',
+        wrapSymbol: '~',
+        tag: 'highlight',
+      }
+    ]
+  };
+
   constructor() {
   }
 
   ngOnInit() {
+
   }
 }
